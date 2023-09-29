38dc043656b1dcde86897bf02e7473d09d7eeaff
Han Chuan <hanchuan.lim@gmail.com>
2023-09-29 21:15:25 +0800

    Initial commmit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button, Pressable } from "react-native"; // pre-2023 uses TouchableOpacity instead of Pressable
+import { useState } from "react";
+import CounterButton from "./components/CounterButton";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function onButtonPressed() {
+    console.log("Button was pressed!");
+    console.log(`Before pressing, the counter was at ${counter}`);
+    setCounter(counter + 1);
+  }
+
+  function renderEncouragingText() {
+    if (counter >= 20) {
+      return "Keep up the good work!";
+    } else if (counter >= 10) {
+      return "Keep Going!";
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{counter}</Text>
+      <CounterButton text="Press Me!" onPress={onButtonPressed} />
+
+      <Text style={styles.encouragingTex}>{renderEncouragingText()}</Text>
     </View>
   );
 }
@@ -13,8 +32,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  encouragingTex: {
+    marginTop: 50,
+    color: "#AAAAAA",
   },
 });
